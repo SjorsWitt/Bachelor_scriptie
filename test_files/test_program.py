313859f00192cb6607073c4293c29518f064c4f5
Sjors Witteveen <sjors@jores.nl>
2017-05-10 12:51:43 +0200

    added extra test functionality

--- test_files/test_program.py
+++ test_files/test_program.py
@@ -3,13 +3,18 @@ c = a + b
 d = 0
 myList = range(10)
 emptyList = []
+thisNameIsGood = True
 thisVariableNameIsWayTooLong = True
 
 def doSomething(param1, param2):
 	param1 += c
 	return param1 + param2
 
+def doAnotherThing(param3):
+	c += 5
+	return param3 - c
+
 something = doSomething(a, c)
 
-for item in myList:
-	d += item
+for i in myList:
+	d += i
