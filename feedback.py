c9f337a2184996842b8b86845777e861d8170413
Sjors Witteveen <sjors@jores.nl>
2017-05-11 18:09:09 +0200

    small adjustments

--- feedback.py
+++ feedback.py
@@ -1,4 +1,4 @@
 tooShortAverageLength = "Your variable names likely do not convey enough meaning, as they are too short on average.\n"
 tooLongAverageLength = "Your variable names are too long on average. Long variable names can be hard to type or can obscure the visual structure of your program.\n"
-singleLetter = "Variable names should not consist of a single letter, unless it is a for loop iterator.\nYou might want to reconsider the following variable names:"
-tooLong = "Variable names of more than 25 characters should be avoided where possible.\nThe following names are too long:"
+singleLetter = "Variable names should not consist of a single letter, unless it is a very simple for loop iterator."
+tooLong = "Variable names of more than 25 characters should be avoided where possible.\nThe following names are too long: "
