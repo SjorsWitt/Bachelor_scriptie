a2e3d2788e13e28a15a95602f9f4e417bfa97723
Sjors Witteveen <sjors@jores.nl>
2017-05-15 18:00:07 +0200

    added some feedback

--- feedback.py
+++ feedback.py
@@ -1,4 +1,7 @@
-tooShortAverageLength = "Your variable names likely do not convey enough meaning, as they are too short on average.\n"
-tooLongAverageLength = "Your variable names are too long on average. Long variable names can be hard to type or can obscure the visual structure of your program.\n"
-singleLetter = "Variable names should not consist of a single letter, unless it is a very simple for loop iterator."
-tooLong = "Variable names of more than 25 characters should be avoided where possible.\nThe following names are too long: "
+TOO_SHORT_AVERAGE = "Your variable names likely do not convey enough meaning, as they are too short on average.\n"
+TOO_LONG_AVERAGE = "Your variable names are too long on average. Long variable names can be hard to type or can obscure the visual structure of your program.\n"
+SINGLE_LETTER = "Variable names should not consist of a single letter, unless it is a very simple for loop iterator.\nYou should reconsider the following names: "
+TOO_LONG = "Variable names of more than 25 characters should be avoided where possible.\nThe following names are too long: "
+
+TOO_MANY_IN_ONE_CLUSTER = "A variable that is used this many times within a small scope should be shorter."
+TOO_LITTLE_IN_ONE_CLUSTER = "A variable that is used this little within a small scope should be more descriptive."
