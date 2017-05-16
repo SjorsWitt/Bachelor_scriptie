57c85acaf03de11058a64f8131697f06185030fd
Sjors Witteveen <sjors@jores.nl>
2017-05-16 16:40:11 +0200

    added new parameters

--- parameters.py
+++ parameters.py
@@ -1,13 +1,13 @@
 # default parameters are based on found literature
-MIN_AVERAGE_NAME_LENGTH = 10
-MAX_AVERAGE_NAME_LENGTH = 16
+MIN_AVERAGE_NAME_LENGTH = 10 #10
+MAX_AVERAGE_NAME_LENGTH = 16 #16
 MAX_NAME_LENGTH = 25
+
+# minimum distance between clusters
+CLUSTER_DISTANCE = 5
 
 # maximum nr of variables an above average name should be in one cluster in the global scope
 GLOBAL_DISTINCTION = 3
 
 # maximum nr of variables an above average name should be in one cluster in a local scope
 LOCAL_DISTINCTION = 2
-
-# minimum distance between clusters
-CLUSTER_DISTANCE = 5
