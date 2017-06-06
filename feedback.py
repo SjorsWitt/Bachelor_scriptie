5d013aef57162a333a1a36574868da5233510a6c
Sjors Witteveen <sjors@jores.nl>
2017-06-06 14:13:03 +0200

    Removed \n from feedback

--- feedback.py
+++ feedback.py
@@ -1,5 +1,5 @@
-TOO_SHORT_AVERAGE = "Your variable names likely do not convey enough meaning, as they are too short on average.\n"
-TOO_LONG_AVERAGE = "Your variable names are too long on average. Long variable names can be hard to type or can obscure the visual structure of your program.\n"
+TOO_SHORT_AVERAGE = "Your variable names likely do not convey enough meaning, as they are too short on average."
+TOO_LONG_AVERAGE = "Your variable names are too long on average. Long variable names can be hard to type or can obscure the visual structure of your program."
 SINGLE_LETTER = "Variable names should not consist of a single letter, unless it is a very simple for loop iterator.\nYou should reconsider the following names: "
 TOO_LONG = "Variable names of more than 25 characters should be avoided where possible.\nThe following names are too long: "
 
