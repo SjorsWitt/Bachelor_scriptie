c93f22bddcea43a1e9a22791ef8650bd75917b4c
Sjors Witteveen <sjors@jores.nl>
2017-06-06 13:33:04 +0200

    adjusted feedback

--- feedback.py
+++ feedback.py
@@ -3,9 +3,13 @@ TOO_LONG_AVERAGE = "Your variable names are too long on average. Long variable n
 SINGLE_LETTER = "Variable names should not consist of a single letter, unless it is a very simple for loop iterator.\nYou should reconsider the following names: "
 TOO_LONG = "Variable names of more than 25 characters should be avoided where possible.\nThe following names are too long: "
 
-TOO_MANY_IN_ONE_CLUSTER = "A variable that is used this many times within a small scope should be shorter to improve readability."
-TOO_LITTLE_IN_ONE_CLUSTER = "A variable that is used this little within a small scope should be more descriptive."
+GLOBAL_TOO_SHORT_SMALL_CLUSTER = "Global variable names should be relatively long, unless it is used frequently within a small piece of code. Short names will likely not convey enough meaning."
+LOCAL_TOO_SHORT_SMALL_CLUSTER = "Local variable names should be longer when the variable is rarely used. Short names will likely not convey enough meaning, while rarely used long names do not decrease the readability of your code."
+TOO_SHORT_BIG_LINE_RANGE = "Variable names should be longer when the variable is used over a longer distance. Short names will lose their meaning over a longer distance."
 
-GLOBAL_BIG_CLUSTER = "Global variable names should be longer than local variables, unless it is used too much within a small piece of code."
-LOCAL_BIG_CLUSTER = "This variable is used too much within a small piece of code and should thus have a shorter name."
-SMALL_LOCAL_SCOPE = "Long names should not be used in local scopes that are relatively small."
+GLOBAL_TOO_LONG_BIG_CLUSTER = "Global variables that are frequently used within a small piece of code should be shorter. Long names that are frequently used within a small piece of code will decrease the readability of your code."
+LOCAL_TOO_LONG_BIG_CLUSTER = "Local variable names should be relatively short, unless it is rarely used. Long names that are frequently used within a small piece of code will decrease readability of your code."
+
+
+EXAMPLE_TOO_SHORT = "Example of such a variable in your code that is too short:"
+EXAMPLE_TOO_LONG = "Example of such a variable in your code that is too long:"
