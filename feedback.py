1530abf4e996b757d540e97aadd6196c36578bd9
Sjors Witteveen <sjors@jores.nl>
2017-06-08 18:09:49 +0200

    made string dependent on parameter

--- feedback.py
+++ feedback.py
@@ -1,7 +1,9 @@
+import parameters
+
 TOO_SHORT_AVERAGE = "Your variable names likely do not convey enough meaning, as they are too short on average."
 TOO_LONG_AVERAGE = "Your variable names are too long on average. Long variable names can be hard to type or can obscure the visual structure of your program."
 SINGLE_LETTER = "Variable names should not consist of a single letter, unless it is a very simple for loop iterator.\nYou should reconsider the following names: "
-TOO_LONG = "Variable names of more than 25 characters should be avoided where possible.\nThe following names are too long: "
+TOO_LONG = "Variable names of more than " + str(parameters.MAX_NAME_LENGTH) + " characters should be avoided where possible.\nThe following names are too long: "
 
 GLOBAL_TOO_SHORT_SMALL_CLUSTER = "Global variable names should be relatively long, unless it is used frequently within a small piece of code. Short names will likely not convey enough meaning."
 LOCAL_TOO_SHORT_SMALL_CLUSTER = "Local variable names should be longer when the variable is rarely used. Short names will likely not convey enough meaning, while rarely used long names do not decrease the readability of your code."
@@ -9,7 +11,6 @@ TOO_SHORT_BIG_LINE_RANGE = "Variable names should be longer when the variable is
 
 GLOBAL_TOO_LONG_BIG_CLUSTER = "Global variables that are frequently used within a small piece of code should be shorter. Long names that are frequently used within a small piece of code will decrease the readability of your code."
 LOCAL_TOO_LONG_BIG_CLUSTER = "Local variable names should be relatively short, unless it is rarely used. Long names that are frequently used within a small piece of code will decrease readability of your code."
-
 
 EXAMPLE_TOO_SHORT = "Example of such a variable in your code that is too short:"
 EXAMPLE_TOO_LONG = "Example of such a variable in your code that is too long:"
