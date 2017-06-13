e43b5f68f5f74e7566fbbb47b496382c68394d6a
Sjors Witteveen <sjors@jores.nl>
2017-06-13 18:50:12 +0200

    added/adjusted some parameters

--- parameters.py
+++ parameters.py
@@ -1,7 +1,7 @@
 # default parameters are based on found literature
 MIN_AVERAGE_NAME_LENGTH = 8 #10
 MAX_AVERAGE_NAME_LENGTH = 14 #16
-MAX_NAME_LENGTH = 20 #25
+MAX_NAME_LENGTH = 23 #25
 
 # minimum distance between clusters
 CLUSTER_DISTANCE = 5
@@ -15,8 +15,11 @@ LOCAL_BIG_CLUSTER = 3
 # variable loses meaning over BIG_DISTANCE number of lines
 BIG_DISTANCE = 10
 
-# exclude iterators when looking for single-letter variables
-EXCLUDE_ITERATORS = True
+# exclude iterators from single-letter variables
+EXCLUDE_ITERATORS_SL = True
+
+# exclude interators from below goal average names
+EXCLUDE_ITERATORS_BA = False
 
 # exclude single-letter variables from below goal average names
-EXCLUDE_SINGLE_LETTER = False
+EXCLUDE_SINGLE_LETTER = True
